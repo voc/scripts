6dd7258305199e4ad8536b8764a8935be8a7187a
Daniel Meißner <daniel@3st.be>
2013-11-04 20:05:45 +0100

    Added munin script to track nginx rtmp clients

---
+++ munin/nginx-rtmp_clients.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'rexml/document'
+require 'net/http'
+
+def get_xml(url)
+  url          = URI.parse(url)
+  http         = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = (url.scheme == 'https')
+  response     = http.start { |http| http.request( Net::HTTP::Get.new(url.path) ) }
+end
+
+def parse_stat_xml(url)
+  xml_data = REXML::Document.new( get_xml(url).body )
+  clients = { }
+  clients[:push] = REXML::XPath.match(xml_data, '//client/publishing').count
+  clients[:pull] = REXML::XPath.match(xml_data, '//client').count - clients[:push]
+end
+
+def get_clients
+  URLS.each_with_index do |url, index|
+    puts "#{index}.value #{parse_stat_xml(url)}"
+  end
+end
+
+# show config
+def show_config
+  puts "graph_title Nginx rtmp clients count"
+  puts 'graph_category nginx'
+  puts 'graph_vlabel Count'
+  URLS.each_with_index do |url, index|
+    puts "#{index}.label #{url}"
+  end
+end
+
+
+if ENV['urls'].size == 0
+  puts "Configure urls:"
+  puts "  [nginx-nrpe_*]"
+  puts "    env.urls = http://example.com/stats/stat.xsl,http://exam.ple/stats/stat.xsl"
+  exit 1
+else
+  URLS = ENV['urls'].split(',')
+end
+
+if ARGV[0] == "config"
+  show_config
+else
+  get_clients
+end
