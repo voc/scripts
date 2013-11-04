84f81c0768ac2e53c754f8360ba503a36c95bb90
Daniel Meißner <daniel@3st.be>
2013-11-04 18:46:23 +0100

    Added option parsing to xml to doku wiki script.

--- xml_talks2doku_wiki.rb
+++ xml_talks2doku_wiki.rb
@@ -1,14 +1,50 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
 
+# This script can be used to create a doku wiki table to track the status of recorded events.
+
 require 'rexml/document'
 require 'net/http'
+require 'optparse'
 
-url          = URI.parse('https://hannover.ccc.de/frab/en/hackover13/public/schedule.xml')
+# option parsing
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{$0} [options]"
+
+  opts.on("-u", "--url URL", String, "Frab url to xml schedule") do |url|
+    options[:url] = url
+  end
+
+  opts.on("-r", "--rooms x,y,z", Array, "List of rooms for pattern matching") do |rooms|
+    options[:rooms] = rooms
+  end
+
+  opts.separator ""
+
+  opts.on_tail("-e", "--example", "Show example script call") do
+    puts "ruby #{$0} -u https://hannover.ccc.de/frab/en/hackover13/public/schedule.xml -r '1\.4,2\.1'"
+    exit
+  end
+
+  opts.on_tail("-h", "--help", "Show this message") do
+    puts opts
+    exit
+  end
+
+  if ARGV.size == 0 || ARGV.size > 4
+    puts opts
+    exit
+  end
+end.parse!
+
+# http schedule request
+url          = URI.parse(options[:url])
 http         = Net::HTTP.new(url.host, url.port)
 http.use_ssl = (url.scheme == 'https')
 response     = http.start {|http| http.request( Net::HTTP::Get.new(url.path) )}
 
+# parse xml
 xml_data   = REXML::Document.new( response.body )
 conference = REXML::XPath.first(xml_data, '//conference/title').text
 days       = REXML::XPath.match(xml_data, '//*/day')
@@ -22,6 +58,12 @@ days.each do |day|
   puts "^ ID ^ Slug ^ h264 ^ webm ^ opus ^ mp3  ^ Comment ^"
 
   day.get_elements('./*/event').each do |event|
-    puts "| #{event.attribute(:id).value} | #{event.get_elements('slug').last.text.gsub(':', '_')} | | | | | |"
+    room =  event.get_elements('./room')[0].text
+    slug =  event.get_elements('slug').last.text.gsub(':', '_')
+    id   =  event.attribute(:id).value
+
+    next unless room =~ /#{options[:rooms].join('|')}/
+
+    puts "| #{id} | #{slug} | | | | | |"
   end
 end
