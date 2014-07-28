369508450d0362d51d98509198f45176dd3af473
MaZderMind <github@mazdermind.de>
2014-07-28 15:59:33 +0200

    a custom pipe-element to trigger the multifilesink

--- snowmix/vocmixer/test-segmenter.py
+++ snowmix/vocmixer/test-segmenter.py
@@ -7,6 +7,9 @@ from pprint import pprint
 gi.require_version('Gst', '1.0')
 from gi.repository import GLib, Gst, Gtk, GObject
 
+GObject.threads_init()
+Gst.init(None)
+
 class Main:
 	def __init__(self):
 		self.pipeline = Gst.Pipeline()
@@ -44,11 +47,17 @@ class Main:
 		self.pipeline.add(self.muxer)
 		self.encoder.link(self.muxer)
 
-		print("filesink")
-		self.filesink = Gst.ElementFactory.make('filesink', 'filesink')
-		self.filesink.set_property("location", "test.ts")
+		print("NewElement")
+		self.newElement = NewElement()
+		self.pipeline.add(self.newElement)
+		self.muxer.link(self.newElement)
+
+		print("multifilesink")
+		self.filesink = Gst.ElementFactory.make('multifilesink', 'filesink')
+		self.filesink.set_property("location", "test-%d.ts")
+		self.filesink.set_property("next-file", 2)
 		self.pipeline.add(self.filesink)
-		self.muxer.link(self.filesink)
+		self.newElement.link(self.filesink)
 
 		print("PLAYING")
 		self.pipeline.set_state(Gst.State.PLAYING)
@@ -65,11 +74,57 @@ class Main:
 			if not sinkpad.is_linked():
 				src_pad.link(sinkpad)
 
+class VocSchnipselsSink(Gst.Element):
+	pass
+
+class NewElement(Gst.Element):
+	""" A basic, buffer forwarding gstreamer element """
+
+	#here we register our plugin details
+	__gstmetadata__ = (
+		"NewElement plugin",
+		"newelement.py",
+		"gst.Element, that passes a buffer from source to sink (a filter)",
+		"Stephen Griffiths <scgmk5@gmail.com>")
+
+	#source pad (template): we send buffers forward through here
+	_srctemplate = Gst.PadTemplate.new('src',
+		Gst.PadDirection.SRC,
+		Gst.PadPresence.ALWAYS,
+		Gst.Caps.new_any())
+
+	#sink pad (template): we recieve buffers from our sink pad
+	_sinktemplate = Gst.PadTemplate.new('sink',
+		Gst.PadDirection.SINK,
+		Gst.PadPresence.ALWAYS,
+		Gst.Caps.new_any())
+
+	#register our pad templates
+	__gsttemplates__ = (_srctemplate, _sinktemplate)
+
+	def __init__(self, *args, **kwargs):
+		#initialise parent class
+		Gst.Element.__init__(self, *args, **kwargs)
+
+		#source pad, outgoing data
+		self.srcpad = Gst.Pad.new_from_template(self._srctemplate, "src")
+
+		#sink pad, incoming data
+		self.sinkpad = Gst.Pad.new_from_template(self._sinktemplate, "sink")
+		self.sinkpad.set_chain_function_full(self._sink_chain, None)
+
+		#make pads available
+		self.add_pad(self.srcpad)
+		self.add_pad(self.sinkpad)
+
+	def _sink_chain(self, pad, parent, buf):
+		#this is where we do filtering
+		#and then push a buffer to the next element, returning a value saying
+		#it was either successful or not.
+		print("buf of ", buf.get_size(), "bytes for pts ", buf.pts)
+		return self.srcpad.push(buf)
 
 def runmain():
-	GObject.threads_init()
-	Gst.init(None)
-
 	signal.signal(signal.SIGINT, signal.SIG_DFL)
 	start=Main()
 	Gtk.main()
