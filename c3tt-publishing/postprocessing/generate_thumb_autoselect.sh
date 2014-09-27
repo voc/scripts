20c04068e9c2cea4c381b319254c8b3facce3bfe
Florian Larysch <fl@n621.de>
2014-09-27 18:54:34 +0200

    thumb_autoselect: use uniform sampling

--- c3tt-publishing/postprocessing/generate_thumb_autoselect.sh
+++ c3tt-publishing/postprocessing/generate_thumb_autoselect.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-CANDIDATES=10
-
 BASEDIR=$(dirname $0)
+INTERVAL=180
 
 INPUT="$1"
 LENGTH=$(ffprobe -loglevel quiet -print_format default -show_format "$INPUT" | grep duration= | sed -e 's/duration=\([[:digit:]]*\).*/\1/g')
 
-# extract candidates and convert to non-anamorphic images
-for i in $(seq 1 $CANDIDATES)
+# now extract candidates and convert to non-anamorphic images
+#
+# we use equidistant sampling, but skip parts of the file that might contain pre-/postroles
+# also, use higher resolution sampling at the beginning, as there's usually some interesting stuff there
+
+for i in 20 30 40 $(seq 15 $INTERVAL $[ $LENGTH - 60 ])
 do
 	POS=$[ $RANDOM % $LENGTH ]
 	ffmpeg -loglevel error -ss $POS -i "$INPUT"  -an -r 1 -filter:v 'scale=sar*iw:ih' -vframes 1 -f image2 -vcodec mjpeg -y $POS.jpg
