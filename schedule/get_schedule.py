971a43d29e2c634e54e1f259723be5852c33a737
derpeter <vocend@derpeter.net>
2014-12-26 17:55:09 +0100

    added fahrplan pull script for 31c3

---
+++ schedule/get_schedule.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+import urllib.request, urllib.parse, urllib.error
+import hashlib 
+import time
+import subprocess
+import datetime
+
+interval = 30
+version_url = "http://events.ccc.de/congress/2014/Fahrplan/version"
+
+version = None
+while True:
+   res = urllib.request.urlopen(version_url)   
+   text = res.read().decode('UTF-8')
+   tmp = str(text).replace('\n',' ')
+   inpos= tmp.rfind('URL: ')
+   outpos = len(tmp)
+   url = tmp[inpos+5:outpos]
+   tmphash = hashlib.md5()
+   tmphash.update(url.encode('UTF-8'))
+   if (tmphash.hexdigest() != version):
+      print(str(datetime.datetime.now()) + " new schedule")
+      version = tmphash.hexdigest()
+      tgz = urllib.request.urlopen(url)
+      target = open('schedule.tar.gz','wb')
+      target.write(tgz.read())
+      try:
+         res = subprocess.check_call(['tar', '-xzf', 'schedule.tar.gz'])
+      except subprocess.CalledProcessError as err:
+         print("error unpacking tar")
+   else:
+      print(str(datetime.datetime.now()) + " no schedule update")
+   time.sleep(interval) 
