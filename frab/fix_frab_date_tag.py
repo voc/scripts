392147bfef6c4a0d720af0c2b22a5a335e4fb8d5
Tobias Preuss <tobias.preuss@googlemail.com>
2016-01-09 23:35:33 +0100

    Add missing seconds to construct valid ISO 8601 date.

--- frab/fix_frab_date_tag.py
+++ frab/fix_frab_date_tag.py
@@ -19,7 +19,8 @@ for day in root.iter('day'):
     day.set('end', date + "T05:00:00+01:00")
     day.set('start', date + "T10:00:00+01:00")
     for event in day.iter('event'):
+        # Append ISO 8601 date; example: 2016-02-29T23:42:00+01:00
         event.append(ET.Element('date'))
-        event.find('date').text = date +  "T" + event.find('start').text + "+01:00"
+        event.find('date').text = date +  "T" + event.find('start').text + ":00+01:00"
 
 tree.write("test.xml")
